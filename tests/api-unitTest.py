e227bada6fd29a0da3d99df1a217c6cceaf4cdf4
Ron Sharabi <ronsh0111@gmail.com>
2023-01-02 20:11:59 +0200

    update tests

--- tests/api-unitTest.py
+++ tests/api-unitTest.py
@@ -1,39 +1,49 @@
 import requests
 
-def signup():
-  try:
-    userData = {'email': 'test@example.com', 'password': 'test123', 'name': 'IAMtest'}
-    return requests.post("http://localhost:3001/api/auth/signup", json = userData)
-  except:
-    raise Exception("Couldn't reach to localhost:3001/api/auth/signup and signup")
-
-def signin():
-  try:
-    return requests.get("http://localhost:3001/api/videos/random")
-  except:
-    raise Exception("Couldn't reach to localhost:3001/api/videos/random")
+URL = "http://localhost:3001/api"
 
 def health():
   try:
-    return requests.get("http://localhost:3001/api/health")
+    return requests.get(f"{URL}/health")
   except:
     raise Exception("Unhealthy.")
 
-def say_hello():
+def signup():
   try:
-    return requests.get("http://localhost:3001/api/say-hello")
+    userData = {'email': 'test@example.com', 'password': 'test123', 'name': 'IAMtest'}
+    return requests.post(f"{URL}/auth/signup", json = userData)
   except:
-    raise Exception("Unhealthy.")
+    raise Exception("Couldn't reach to localhost:3001/api/auth/signup and signup")
+
+def get_videos():
+  try:
+    return requests.get(f"{URL}/videos/random")
+  except:
+    raise Exception("Couldn't reach to localhost:3001/api/videos/random")
+
+def get_test():
+  try:
+    return requests.get(f"{URL}/test")
+  except:
+    raise Exception("Couldn't reach to localhost:3001/api/test")
+
+
+data = health()
+print(data.content)
+if data.status_code != 200:
+  raise Exception(f"Getting health went wrong. status_code: {data.status_code}")
 
 data = signup()
 print(data.content)
 if data.status_code != 200:
   raise Exception(f"Signup went wrong. status_code: {data.status_code}")
 
-data = signin()
+data = get_videos()
 print(data.content)
 if data.status_code != 200:
   raise Exception(f"Getting videos/random went wrong. status_code: {data.status_code}")
 
-health()
-say_hello()
+data = get_test()
+print(data.content)
+if data.status_code != 200:
+  raise Exception(f"Getting test went wrong. status_code: {data.status_code}")
